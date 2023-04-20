8a8f83353f269b7477cd9d17634da91551a14597
xiehui <xie18960920183@outlook.com>
2023-04-20 21:23:09 +0800

    docs: 添加dal.go 注释

--- gendemo/dal/dal.go
+++ gendemo/dal/dal.go
@@ -11,17 +11,23 @@ import (
 	"github.com/ag9920/db-demo/gendemo/dal/model"
 )
 
+/*
+ * 只是维护了内存中的数据库连接，完成初始化，和业务无关。
+ */
 var DB *gorm.DB
 var once sync.Once
 
 func init() {
+	// 只会执行一次
 	once.Do(func() {
 		DB = ConnectDB().Debug()
+		// 自动迁移
 		_ = DB.AutoMigrate(&model.User{}, &model.Passport{})
 	})
 }
 
 func ConnectDB() (conn *gorm.DB) {
+	//  打开数据库连接
 	conn, err := gorm.Open(sqlite.Open(":memory:"), &gorm.Config{})
 	if err != nil {
 		panic(fmt.Errorf("cannot establish db connection: %w", err))
